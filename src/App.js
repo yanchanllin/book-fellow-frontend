cb1ef7adc87f20c180bbe4615073022b88a117d7
yanchanllin <yonalin1@gmail.com>
2019-06-18 12:08:40 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login.js";
+import Logout from "./components/Logout.js";
 import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser.js";
-import NavBar from "./components/NavBar.js";
-import MainContainer from "./components/MainContainer.js";
 
 class App extends React.Component {
   componentDidMount() {
@@ -11,16 +11,17 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <MainContainer />
-      </div>
-    );
+    return this.props.currentUser ? <Logout /> : <Login />;
   }
 }
 
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { getCurrentUser }
 )(App);
