fb5b35a1d82bc801d68b14d4c752b98295864b6b
yanchanllin <yonalin1@gmail.com>
2019-06-20 13:23:10 -0700

    add mybooks for maincontainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MyBooks from "./MyBooks.js";
 
 const MainContainer = () => {
-  return <div className="MainContainer" />;
+  return (
+    <div className="MainContainer">
+      <MyBooks />
+    </div>
+  );
 };
 
 export default MainContainer;
