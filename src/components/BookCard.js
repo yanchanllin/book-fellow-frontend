157cbc6796b62299eaad34d4b8a206e9c06496bd
yanchanllin <yonalin1@gmail.com>
2019-06-25 13:24:21 -0700

    update book description in bookcard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const BookCard = ({ book }) => {
-  return <p>{book.attributes.name}</p>;
+  return (
+    <p>
+      <b>Title:{book.attributes.name}</b> by:
+      {book.attributes.author}
+      <br /> [{book.attributes.description}]
+    </p>
+  );
 };
 
 export default BookCard;
+
+// css for books
