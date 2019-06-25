720915ded3f05967509d308c618a2a0cf6d46253
yanchanllin <yonalin1@gmail.com>
2019-06-25 13:25:03 -0700

    add my review in maincontainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MyBooks from "./MyBooks.js";
+import MyReviews from "./MyReviews.js";
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
       <MyBooks />
+      <MyReviews />
     </div>
   );
 };
