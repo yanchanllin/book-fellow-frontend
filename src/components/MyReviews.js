428e17be0fe444d44c8dd14f1ada78f337328c63
yanchanllin <yonalin1@gmail.com>
2019-06-28 11:45:00 -0700

    update reviews

--- src/components/MyReviews.js
+++ src/components/MyReviews.js
@@ -1,23 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import ReviewCard from "./ReviewCard.js";
 import { connect } from "react-redux";
 import { getMyReviews } from "../actions/myReviews.js";
-import MyBooks from "./MyBooks.js";
 
-class Review extends Component {
-  componentDidMount = () => {
-    this.props.getMyReviews(book_id);
-  };
-  render() {
-    return (
-      <div className="myReviews">
-        <ReviewCard reviews={this.props.reviews(book_id)} />
-      </div>
-    );
-  }
-}
-
-const Reviews = props => {
+const MyReviews = props => {
   const reviewCards =
     props.reviews.length > 0
       ? props.reviews.map(r => <ReviewCard review={r} key={r.id} />)
@@ -34,4 +20,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   { getMyReviews }
-)(Reviews);
+)(MyReviews);
