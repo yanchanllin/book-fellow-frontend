acd3ffe763981eadc8c1b254e2c201af6c54dff2
yanchanllin <yonalin1@gmail.com>
2019-06-28 11:44:25 -0700

    delete MyReview component don't need it

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,11 @@
 import React from "react";
 import MyBooks from "./MyBooks.js";
-import MyReviews from "./MyReviews.js";
 
 const MainContainer = () => {
   return (
     <div className="MainContainer">
       <MyBooks />
-      <MyReviews />
+      {/* <BookForm /> */}
     </div>
   );
 };
