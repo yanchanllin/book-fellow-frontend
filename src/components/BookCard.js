f5e274790105d9e4bcfe3ea9a1f258365cc04929
yanchanllin <yonalin1@gmail.com>
2019-09-23 14:17:49 -0700

    adding class to bookcard.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ReviewCard from "./ReviewCard.js";
 
-const BookCard = ({ book, deleteBook }) => {
-  return book ? (
-    <div>
-      <h3> Title: "{book.attributes.name}" </h3>
-      <li>
-        By: {book.attributes.author} <br />
-        [Description: {book.attributes.description}]
+class BookCard extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    const book = this.props.book
+    const deleteBook = this.props.deleteBook
+    return book ? (
+      <div>
+        <h3> Title: "{book.attributes.name}" </h3>
+        <li>
+          By: {book.attributes.author} <br />
+          [Description: {book.attributes.description}]
         {/* Review:{" "} */}
-        <ReviewCard reviews={book.attributes.reviews} />
-      </li>
-      <br />
-      <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
-      <br />
-      <button onClick={() => deleteBook(book.id)}>DELETE</button>
-    </div>
-  ) : (
-      <p>This the the BOOK card with no BOOK!</p>
-    );
+          <ReviewCard reviews={book.attributes.reviews} />
+        </li>
+        <br />
+        <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+        <br />
+        <button onClick={() => deleteBook(book.id)}>DELETE</button>
+      </div>
+    ) : (
+        <p>This the the BOOK card with no BOOK!</p>
+      );
+  }
 };
 
 export default BookCard;
 
 //This page is for css of books
-//going to add a handlesubmit and handlechange to add review here
+//going to add a handlesubmit and handlechange to add review here not finish yet 
