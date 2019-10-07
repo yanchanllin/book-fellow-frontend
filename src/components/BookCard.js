6a6990524258a1482792a1dfe625869e6455a070
yanchanllin <yonalin1@gmail.com>
2019-10-07 15:07:56 -0700

    delete review card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,34 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import ReviewCard from "./ReviewCard.js";
 
-class BookCard extends React.Component {
-  constructor() {
-    super()
-  }
-
-  render() {
-    const book = this.props.book
-    const deleteBook = this.props.deleteBook
-    return book ? (
-      <div>
-        <h3> Title: "{book.attributes.name}" </h3>
-        <li>
-          By: {book.attributes.author} <br />
-          [Description: {book.attributes.description}]
-        {/* Review:{" "} */}
-          <ReviewCard reviews={book.attributes.reviews} />
+const BookCard = ({ book, deleteBook }) => {
+  return book ? (
+    <div>
+      <h3> Title: "{book.attributes.name}" </h3>
+      <li>
+        By: {book.attributes.author} <br />
+        [Description: {book.attributes.description}]
         </li>
-        <br />
-        <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
-        <br />
-        <button onClick={() => deleteBook(book.id)}>DELETE</button>
-      </div>
-    ) : (
-        <p>This the the BOOK card with no BOOK!</p>
-      );
-  }
-};
+      <br />
+      <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+      <br />
+      <button onClick={() => deleteBook(book.id)}>DELETE</button>
+    </div>
+  ) : (
+      <p>This the the BOOK card with no BOOK!</p>
+    );
+}
 
 export default BookCard;
 
