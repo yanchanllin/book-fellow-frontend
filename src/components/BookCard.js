56ae6fa658658954ee14bd8aaf099c5b51353f4e
yanchanllin <yonalin1@gmail.com>
2019-10-09 16:15:32 -0700

    add comment input for book card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,25 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const BookCard = ({ book, deleteBook }) => {
-  return book ? (
-    <div>
-      <h3> Title: "{book.attributes.name}" </h3>
-      <li>
-        By: {book.attributes.author} <br />
-        [Description: {book.attributes.description}]
+class BookCard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      comments: [],
+      value: ""
+    }
+  }
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      comments: this.state.comments.concat(this.state.value),
+      value: ""
+    })
+  }
+  render() {
+    const { book, deleteBook } = this.props
+    return book ? (
+      <>
+        <div>
+          <h3> Title: "{book.attributes.name}" </h3>
+          <li>
+            By: {book.attributes.author} <br />
+            [Description: {book.attributes.description}]
         </li>
-      <br />
-      <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
-      <br />
-      <button onClick={() => deleteBook(book.id)}>DELETE</button>
-    </div>
-  ) : (
-      <p>This the the BOOK card with no BOOK!</p>
-    );
+          <br />
+          <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+          <br />
+          <button onClick={() => deleteBook(book.id)}>DELETE</button>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.value}
+            onChange={this.handleChange} />
+          <input type="Submit" value="add comment" />
+        </form>
+        {this.state.comments.map(comment => (
+          <p>{comment}</p>
+        ))}
+      </>
+    ) : (
+        <p>This the the BOOK card with no BOOK!</p>
+      );
+  }
 }
 
-export default BookCard;
-
-//This page is for css of books
-//going to add a handlesubmit and handlechange to add review here not finish yet. 
+export default BookCard; 
