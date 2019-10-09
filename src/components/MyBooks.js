402a91d671e22f8830485496f7364aad9e0bc99c
yanchanllin <yonalin1@gmail.com>
2019-10-09 16:18:57 -0700

    clean up this file

--- src/components/MyBooks.js
+++ src/components/MyBooks.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 
 class MyBooks extends React.Component {
-
   render() {
     const bookCards = this.props.books.map((b) => (
       <>
